8a3106696729dce4f847a4933169e27626baf0ba
PavelK <passapdom@gmail.com>
2024-03-16 00:37:43 +0300

    add

---
+++ Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.buttonDiam = new System.Windows.Forms.Button();
+            this.buttonVolume = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonClean = new System.Windows.Forms.Button();
+            this.button_enter = new System.Windows.Forms.Button();
+            this.buttonInput = new System.Windows.Forms.Button();
+            this.buttonPrint = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.buttonClearList = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(241, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите радиус основания конуса: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(176, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Введиете высоту конуса: ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(283, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(283, 98);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // buttonDiam
+            // 
+            this.buttonDiam.Location = new System.Drawing.Point(39, 187);
+            this.buttonDiam.Name = "buttonDiam";
+            this.buttonDiam.Size = new System.Drawing.Size(168, 30);
+            this.buttonDiam.TabIndex = 4;
+            this.buttonDiam.Text = "рассчитать диаметр";
+            this.buttonDiam.UseVisualStyleBackColor = true;
+            this.buttonDiam.Click += new System.EventHandler(this.buttonDiam_Click);
+            // 
+            // buttonVolume
+            // 
+            this.buttonVolume.Location = new System.Drawing.Point(39, 241);
+            this.buttonVolume.Name = "buttonVolume";
+            this.buttonVolume.Size = new System.Drawing.Size(167, 32);
+            this.buttonVolume.TabIndex = 5;
+            this.buttonVolume.Text = "рассчитать объем";
+            this.buttonVolume.UseVisualStyleBackColor = true;
+            this.buttonVolume.Click += new System.EventHandler(this.buttonVolume_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(520, 293);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(522, 349);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(315, 296);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(199, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "полученный диаметр конуса:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(315, 355);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(185, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "полученный объем конуса:";
+            // 
+            // buttonClean
+            // 
+            this.buttonClean.Location = new System.Drawing.Point(45, 323);
+            this.buttonClean.Name = "buttonClean";
+            this.buttonClean.Size = new System.Drawing.Size(135, 48);
+            this.buttonClean.TabIndex = 10;
+            this.buttonClean.Text = "очистить ввод и вывод";
+            this.buttonClean.UseVisualStyleBackColor = true;
+            this.buttonClean.Click += new System.EventHandler(this.buttonClean_Click);
+            // 
+            // button_enter
+            // 
+            this.button_enter.Location = new System.Drawing.Point(39, 147);
+            this.button_enter.Name = "button_enter";
+            this.button_enter.Size = new System.Drawing.Size(167, 23);
+            this.button_enter.TabIndex = 11;
+            this.button_enter.Text = "Ввести данные";
+            this.button_enter.UseVisualStyleBackColor = true;
+            this.button_enter.Click += new System.EventHandler(this.button_enter_Click);
+            // 
+            // buttonInput
+            // 
+            this.buttonInput.Location = new System.Drawing.Point(559, 31);
+            this.buttonInput.Name = "buttonInput";
+            this.buttonInput.Size = new System.Drawing.Size(216, 37);
+            this.buttonInput.TabIndex = 12;
+            this.buttonInput.Text = "сохранить и расчитать";
+            this.buttonInput.UseVisualStyleBackColor = true;
+            this.buttonInput.Click += new System.EventHandler(this.buttonInput_Click);
+            // 
+            // buttonPrint
+            // 
+            this.buttonPrint.Location = new System.Drawing.Point(562, 92);
+            this.buttonPrint.Name = "buttonPrint";
+            this.buttonPrint.Size = new System.Drawing.Size(213, 35);
+            this.buttonPrint.TabIndex = 13;
+            this.buttonPrint.Text = "вывод списка";
+            this.buttonPrint.UseVisualStyleBackColor = true;
+            this.buttonPrint.Click += new System.EventHandler(this.buttonPrint_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(283, 149);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(492, 116);
+            this.listBox1.TabIndex = 14;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // buttonClearList
+            // 
+            this.buttonClearList.Location = new System.Drawing.Point(641, 398);
+            this.buttonClearList.Name = "buttonClearList";
+            this.buttonClearList.Size = new System.Drawing.Size(134, 40);
+            this.buttonClearList.TabIndex = 15;
+            this.buttonClearList.Text = "очистить список";
+            this.buttonClearList.UseVisualStyleBackColor = true;
+            this.buttonClearList.Click += new System.EventHandler(this.buttonClearList_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonClearList);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.buttonPrint);
+            this.Controls.Add(this.buttonInput);
+            this.Controls.Add(this.button_enter);
+            this.Controls.Add(this.buttonClean);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.buttonVolume);
+            this.Controls.Add(this.buttonDiam);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button buttonDiam;
+        private System.Windows.Forms.Button buttonVolume;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonClean;
+        private System.Windows.Forms.Button button_enter;
+        private System.Windows.Forms.Button buttonInput;
+        private System.Windows.Forms.Button buttonPrint;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button buttonClearList;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        private List<konus_class> Cone_List = new List<konus_class>();
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeCone();
+
+            textBox1.Enabled = false;
+            textBox2.Enabled = false;
+        }
+
+        konus_class Not_name;
+
+        private void InitializeCone()
+        {
+            for (int i = 0; i < 16; i++)
+            {
+                Cone_List.Add(new konus_class(0, 0));
+            }
+        }
+
+        private bool flag = false;
+        private int activeIndex = -1;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// A class for calculating the diameter and volume of a straight cone.
+        /// </summary>
+        public class konus_class
+        {
+            private double radius;
+            private double height;
+
+            public konus_class(double radius, double height)
+            {
+                this.radius = radius; this.height = height;
+            }
+            public konus_class(double radius)
+            {
+                this.radius = radius;
+            }
+            public konus_class()
+            {
+                radius = 0;
+                height = 0;
+            }
+            public double Calcul_Diameter()
+            {
+                return 2 * radius;
+            }
+
+            public double Calcul_Volume()
+            {
+                return (Math.PI * Math.Pow(radius, 2) * height) / 3;
+            }
+            public void SetRadius(double rad)
+            {
+                if (rad < 0)
+                {
+                    MessageBox.Show("Введите не отрицательное число");
+                }
+                else 
+                {
+                    radius = rad;
+                }
+            }
+            public void SetHeight( double len_height) 
+            {
+                if (len_height < 0)
+                {
+                    MessageBox.Show("Введите не отрицательное число");
+                }
+                if (len_height < 9 || len_height > 30)
+                {
+                    MessageBox.Show("Введите число больше 9 и меньше 30 (для высоты) ");
+                }
+                else
+                {
+                    height = len_height;
+                }
+            }
+            public double GetRadius()
+            {
+                return radius;
+            }
+            public double GetHeight()
+            {
+                return height;
+            }
+        }
+
+
+        private void buttonDiam_Click(object sender, EventArgs e)
+        {
+            if (!flag)
+            {
+                textBox3.Text = Not_name.Calcul_Diameter().ToString();
+                return;
+            }
+
+            double radius;
+            if (double.TryParse(textBox1.Text, out radius))
+            {
+                if (radius > 0)
+                {
+                    konus_class cone = new konus_class(radius);
+                    textBox3.Text = cone.Calcul_Diameter().ToString();
+                }
+                else
+                {
+                    MessageBox.Show("Введено некорректное значение радиуса");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Не введено значение радиуса");
+            }
+        }
+
+        private void buttonVolume_Click(object sender, EventArgs e)
+        {
+            if (!flag)
+            {
+                textBox4.Text = Not_name.Calcul_Volume().ToString();
+                return;
+            }
+
+            double radius, height;
+            if (double.TryParse(textBox1.Text, out radius) && double.TryParse(textBox2.Text, out height))
+            {
+                if (radius >= 0)
+                {
+                    if (height > 9 && height < 30)
+                    {
+                        konus_class cone = new konus_class(radius, height);
+                        textBox4.Text = cone.Calcul_Volume().ToString();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Значение высоты должно быть от 9 до 30");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Введите неотрицательное значение для радиуса");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Не все значения введены");
+            }
+        }
+
+        private void buttonClean_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            
+        }
+
+        private void button_enter_Click(object sender, EventArgs e)
+        {
+            flag = true;
+            textBox1.Enabled = true;
+            textBox2.Enabled = true;
+        }
+
+        private void buttonInput_Click(object sender, EventArgs e)
+        {
+            if (activeIndex != -1)
+            {
+                double radius, height;
+                if (double.TryParse(textBox1.Text, out radius) && double.TryParse(textBox2.Text, out height))
+                {
+                    Cone_List[activeIndex].SetRadius(radius);
+                    Cone_List[activeIndex].SetHeight(height);
+                    listBox1.Items[activeIndex] = $"Конус {activeIndex + 1}: Радиус - {radius}, Высота - {height}, Диаметр - {Cone_List[activeIndex].Calcul_Diameter()}, Объем - {Cone_List[activeIndex].Calcul_Volume()}";
+                }
+                else
+                {
+                    MessageBox.Show("Некорректные значения радиуса или высоты");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Выберите объект из списка для сохранения изменений");
+            }
+        }
+
+        private void buttonPrint_Click(object sender, EventArgs e)
+        {
+ 
+            int index = 0;
+            foreach (konus_class cone in Cone_List)
+            {
+                listBox1.Items.Add($"Конус {index + 1}: Радиус - {cone.GetRadius()}, Высота - {cone.GetHeight()}, Диаметр - {cone.Calcul_Diameter()}, Объем - {cone.Calcul_Volume()}");
+                index++;
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            activeIndex = listBox1.SelectedIndex;
+            if (activeIndex >= 0 && activeIndex < Cone_List.Count)
+            {
+                textBox1.Text = Cone_List[activeIndex].GetRadius().ToString();
+                textBox2.Text = Cone_List[activeIndex].GetHeight().ToString();
+            }
+        }
+
+        private void buttonClearList_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            Cone_List.Clear();
+            for (int i = 0; i < 16; i++)
+            {
+                Cone_List.Add(new konus_class(0, 0));
+            }
+        }
+    }
+}
